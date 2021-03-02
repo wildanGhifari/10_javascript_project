65dcdfd7d71d3d1b513ddcd64c80e9d54f78a4ee
wildanGhifari <wildghifari@gmail.com>
2021-03-02 23:49:15 +0700

    Create Quiz App and make a few Questions

---
+++ quiz-app/app.js
@@ -0,0 +1,29 @@
+const quizDate = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        exmaple: undefined,
+        a: '<javascript>',
+        b: '<js>',
+        c: '<scripting>',
+        d: '<script>',
+        correct: 'd'
+    },
+    {
+        question: 'What is the correct JavaScript syntax to change the content of the HTML element below?',
+        exmaple: '<p id="demo">This is a demonstration.</p>',
+        a: 'document.getElement("p").innerHTML = "Hello World!"',
+        b: 'document.getElementById("demo").innerHTML = "Hello World!',
+        c: '#demo.innerHTML = "Hello World!"',
+        d: 'document.getElementByName("p").innerHTML = "Hello World!"',
+        correct: 'b'
+    },
+    {
+        question: 'How do you write "Hello World" in an alert box?',
+        exmaple: undefined,
+        a: 'alert("Hello World!")',
+        b: 'alertBox("Hello World!")',
+        c: 'msgBox("Hello World!")',
+        d: 'msg("Hello World")',
+        correct: 'a'
+    }
+]
